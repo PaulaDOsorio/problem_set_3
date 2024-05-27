a2ab5af694667ad27074494ec3f0c9de2be20a2c
isierrao <i.sierrao@uniandes.edu.co>
2024-05-26 22:36:53 -0500

    red neuronal
    con otros hiperparametros

--- scripts/modelos 2.R
+++ scripts/modelos 2.R
@@ -14,3 +14,41 @@ modelo_glmboost <- train(
 # Visualizar el resumen del modelo
 summary(modelo_glmboost)
 
+ #entrenar modelo de red neuronal 
+modelo_nnet <- train(
+  formula = price ~ bedrooms + bathrooms + rooms + ascensor + patio + remodel + parqueo + piso +
+    as.factor(year) + as.factor(property_type) + iluminado + distancia_parque +
+    distancia_escuela + distancia_estacion + distancia_comercial + distancia_banco +
+    distancia_bus + ESTRATO,
+  data = train_final,
+  method = "nnet",
+  trControl = cv,
+  linout = TRUE,     # Para regresión en lugar de clasificación
+  trace = FALSE,     # Para no mostrar el proceso de entrenamiento
+  tuneLength = 5     # Para buscar los mejores hiperparámetros
+)
+ 
+
+# Definir el grid de hiperparámetros
+tune_grid <- expand.grid(
+  size = c(5, 10, 15),  # Número de neuronas en la capa oculta
+  decay = c(0.001, 0.01, 0.1)  # Tasa de decaimiento
+)
+
+# Entrenar el modelo de red neuronal
+modelo_nnet <- train(
+  formula = price ~ bedrooms + bathrooms + rooms + ascensor + patio + remodel + parqueo + piso +
+    as.factor(year) + as.factor(property_type) + iluminado + distancia_parque +
+    distancia_escuela + distancia_estacion + distancia_comercial + distancia_banco +
+    distancia_bus + ESTRATO,
+  data = train_final,
+  method = "nnet",
+  trControl = cv_control,
+  tuneGrid = tune_grid,
+  linout = TRUE,     # Para regresión en lugar de clasificación
+  trace = FALSE      # Para no mostrar el proceso de entrenamiento
+)
+
+# Resumen del modelo
+print(modelo_nnet)
+
